0248203b926ae6316476abe35fc3e0f825858e36
suryakochirajavu <tornadodigitalmarketing@gmail.com>
2025-05-16 13:23:25 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,6 @@ export const metadata: Metadata = {
   description:
     "Portfolio of Sangeeth P Girish, a digital artist and creative director specializing in digital art, motion graphics, and brand identity.",
   generator: 'v0.dev',
-  icons: {
-    icon: "/favicon.png",
-  },
 }
 
 export default function RootLayout({
@@ -25,8 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.png" type="image/png" />
-      </head>
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  <link rel="manifest" href="/site.webmanifest" />
+  <link rel="shortcut icon" href="/favicon.ico" />
+</head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <EnhancedLayout>{children}</EnhancedLayout>
